473886c1897f6425890cc2dfa0e307e2686bdf43
Daniel <daniel_guy_98@hotmail.co.uk>
2019-11-26 22:00:02 +0000

    Complete with comments

--- Assignment/Code/Task1.py
+++ Assignment/Code/Task1.py
@@ -1,204 +1,134 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
-#import numpy.linalg as linalg
 
+##Get Polynomial Data Matrix given coordinates and degree
 def get_poly_data_matrix(x, degree):
     X = np.ones(x.shape)
     for i in range(1, degree+1):
         X = np.column_stack((X, x**i))
-#    print(X)
     return X
 
 
-
+##Get Weights/Parameters given coorinates and polynomial degree
 def get_weights(x,y,degree):
     X = get_poly_data_matrix(x,degree)
     XX  = X.transpose().dot(X)
     weight = np.linalg.solve(XX, X.transpose().dot(y))
-
-#    weight = np.polyfit(x,y,degree)
-
     return weight
 
 
 
-
-
+##Calculating the Root Mean Square Error, given
 def eval_pol_regression(parameters, x, y, degree):
     rmse = 0
 
     ##Squaring the residuals
     ##Finding the average
     ##Taking the square root of the result
-
+    
+    #ŷ is the predicted value of y. ie - the y based on the parameters given
     yHat = 0
-#    print("Maths:")
-#    print(x)
-#    for i in x:
-#        yHat = np.polyval(parameters,i)
-#        print(yHat)
 
-#    for i in range(0, len(x)):
-#        yHat = np.polyval(parameters,x[i])
-##        print(yHat)
-##        print(y[i])
-#        sumYHat.append(yHat)
-##        print((yHat-y[i])**2)
-#        sumVals += (yHat-y[i])**2
-##        sumVals += (y[i]-yHat)**2
-#    print(sumYHat)
-#    print("Sum Vals / n")
-#    print(sumVals/len(x))
-#    print("Sqrt: ")
-#    print(np.sqrt(sumVals/len(x)))
-#    rmse = np.sqrt(sumVals/len(x))
+    #Calculating predicted values for all of x
+    for i in range(0, len(parameters)):                 #For each parameter:
+        yHat += (parameters[i])*(x**i)                  #calculate ŷ = bi(x)^i
     
-    sumYHat = [None]*len(x)
-    for i in range(0, len(parameters)):
-#        print(parameters[i])
-        yHat += (parameters[i])*(x**i)
-        print("{0}{1}^{2}".format(parameters[i],x,i))
-#        print("yHat")
-#        print(yHat)
-    print("Done")
-    print(yHat)
-    sumYHat = yHat
-    
-    
-#    print("sumYHat")
-#    print(sumYHat)
-#    print("Y")
-#    print(y)
-#        print(sum_residuals)
-#    print("SE:")
-#    print("Sum")
-#    print(sumYHat[0])
-#    print(y)
-    ##Sum of all (yHat - Y)
-    se = sumYHat - y
-#    print(sum_residuals - y)
-#    print(y - sum_residuals)
-#    se = y - sum_residuals
-#    for i in se:
-#        rmse += i
-#    for i in x:
-#        rmse += parameters[len(parameters)-1]*i
-##    print(rmse)
-#    rmse = rmse**2
-##    print(len(x))
-#    rmse = rmse / len(x)
-##    print(rmse)
-#    rmse = np.sqrt(rmse)
-    
-    rmse = np.sqrt(((sumYHat - y)**2).mean())
-    print("RMSE")
-    print(rmse)
-#    rmse = np.sqrt(mean_squared_error(y, sumYHat))
-    
-    
-    
+    xMatrix = get_poly_data_matrix(x,degree)
+    #Calculate Root Mean Square Error
+    rmse = np.sqrt(((xMatrix.dot(parameters)-y)**2).mean())
     return rmse
 
 
 
-
-
+##Plot the Data
 def plot_data(axs, sX, para):
-    y_coords = []
-    for x in np.arange(-5,5,0.1):
-        sumY = 0
-        for b in range(len(para)):
-        #        exp = len(para)-(b+1)
-            exp = b
-            coeff = para[b]
-        #        print("{0}X^{1}".format(coeff,exp))
-            sumY += coeff * (x**exp)
-#            print(sumY)
-        y_coords.append(sumY)
-    axs[sX].plot(np.arange(-5,5,0.1),y_coords)
+    #Get matrix of x coordinates based on x = -5 -> 5 (step 0.1).
+    xCoords = get_poly_data_matrix(np.arange(-5,5,0.1),len(para)-1)
+    yCoords = xCoords.dot(para)
+    axs[sX].plot(np.arange(-5,5,0.1),yCoords)
     return
 
 
-
+##Polynomial Regression
 def pol_regression(x_train, y_train, degree):
-    #Code
     parameters = 0
-#    print(coeffs)
-#    print(coeffs[0])
+    ##Ensures degree is larger than 0 to avoid errors
     if degree > 0:
-        w = get_weights(x_train, y_train, degree)
+        parameters = get_weights(x_train, y_train, degree)
         Xtest = get_poly_data_matrix(x_train, degree)
-        ytest = Xtest.dot(w)
-        x_test, y_test = sort_data(x_train, ytest)
-#        plt.plot(x_test, y_test)
-        parameters = w
+        ytest = Xtest.dot(parameters) #Dot product of X coord matrix against the weights
+        x_test, y_test = sort_data(x_train, ytest)      
 #    else:        
 #        plt.plot([-5,5],[0,0])
-    
-    
     return parameters
-
 
 
 
 #Get Data
 def get_data():
+    ##Import data from csv file
     data = pd.read_csv('data_pol_regression.csv')
-#    print(data)
+    
+    #Randomise the data
     data = data.sample(frac=1)
-#    print(data)
     return data
 
 
 
-
-
-def sort_data(x_in, y_in):
 ##Sort Data
-        df = pd.DataFrame([x_in, y_in])
-        df.sort_values(0, axis=1, ascending = True, inplace = True)
-        x = []
-        y = []
-        for point in df:
-            x = np.append(x, df[point][0])
-            y = np.append(y, df[point][1])
-        return x, y
+def sort_data(x_in, y_in):
+    #Creates a dataframe of x and y coordinates
+    df = pd.DataFrame([x_in, y_in])
+    
+    #Use the data frames prebuilt sorting function to sort the data in order of x coordinates
+    df.sort_values(0, axis=1, ascending = True, inplace = True)
+    
+    #Initialise coordinate arrays
+    x = []
+    y = []
+    
+    #For every point in the dataframe, append each coord list with its respective coordinate
+    for point in df:
+        x = np.append(x, df[point][0])
+        y = np.append(y, df[point][1])
+    return x, y
 
 
 
-
-
-
+##Get Training Data
 def get_training_data(data, split):
+    #Initialse X and Y coords from the data
     x_input = data['x'].as_matrix()
     y_input = data['y'].as_matrix() 
-#    print(len(x_input)*split)
-    test_size = round((len(x_input)*split))
-#    print("Training Data:")
+    
+    #Determines the size of traning data set based on the float parsed
+    training_size = round((len(x_input)*split))
+    
+    #Initialises x and y training coordinate arrays
     x_train = []
     y_train = []
-    for i in range(test_size):
-#        print("X:{0} : Y:{1}".format(x_input[i],y_input[i]))
+    
+    #Appends coordinate points to the array
+    for i in range(training_size):
         x_train = np.append(x_train, x_input[i])
         y_train = np.append(y_train, y_input[i])
-#    print("Test Data:")
+    
+    #Sorts data into order 
     x_train, y_train = sort_data(x_train, y_train)
         
+    #Initialises x and y testing coordinate arrays
     x_test = []
     y_test = []
-    if split != 1:    
-        
-        for i in range(test_size,len(data)):
-    #        print("X:{0} : Y:{1}".format(x_input[i],y_input[i]))
+    
+    #If the split isn't 100% training, put remaining coordinates into the test arrays
+    if split != 1:
+        for i in range(training_size,len(data)):
             x_test = np.append(x_test, x_input[i])
             y_test = np.append(y_test, y_input[i])
-    x_test, y_test = sort_data(x_test, y_test)
-    
-    
+        #Sort the data
+        x_test, y_test = sort_data(x_test, y_test)
     return x_train, y_train, x_test, y_test
-
 
 
 
@@ -208,37 +138,44 @@ x_train, y_train, x_test, y_test = get_training_data(data, 1)
 degrees = [0,1,2,3,5,10]
 parameters = [None]*6
 
-####    Plotting Data    ####
+####    Initialising Graphs Data    ####
 fig, axs = plt.subplots(2)
-
-#plt.figure()
 axs[0].set_xlim(-5,5)
 axs[0].set_ylim(-200,50)
-#axs[1].set_ylim(0,10)
 axs[1].set_yscale('log')
+#axs[1].set_ylim(0,10)
+
+
+##Plotting *all* given data
 axs[0].plot(x_train, y_train, 'bo')
 
+##Plotting polynomials of degree 1,2,3,5,10
 for i in range(1,len(degrees)):
     parameters[i] = pol_regression(x_train, y_train, degrees[i])
     plot_data(axs,0,parameters[i])
-axs[0].legend(["Data",0,1,2,3,5,10],bbox_to_anchor=(1.05,1))
+axs[0].legend(['Ground Truth','$x^{1}$','$x^{2}$','$x^{3}$','$x^{5}$','$x^{10}$'],bbox_to_anchor=(1.05,1))
 
+
+#Training and Testing data
 x_train, y_train, x_test, y_test = get_training_data(data, 0.7)
 axs[1].set(xlabel='Degree', ylabel='RSME')
 
-#eval_pol_regression(parameters[2],x_train,y_train, (len(parameters[2])-1))
-
-#deg0 = pol_regression(x_train, y_train, 0)
-print(deg0)
+####    Show data with only 70% training data    ####
 #for i in range(1,len(degrees)):
 #    parameters[i] = pol_regression(x_train, y_train, degrees[i])
 #    plot_data(axs,0,parameters[i])
+
+##Initialise the test and training rsme lists
 rsme_Train = [None]*6
 rsme_Test = [None]*6
-#print(len(x_train))
+
+##For each degree, determine parameters for those lists, then calculates the RMSE for the training and test data.
 for i in range(1,len(degrees)):
+    parameters[i] = get_weights(x_train,y_train,degrees[i])
     rsme_Train[i] = eval_pol_regression(parameters[i], x_train, y_train, (len(parameters[i])-1))
     rsme_Test[i] = eval_pol_regression(parameters[i], x_test, y_test, (len(parameters[i])-1))
+
+##Plot Data
 axs[1].plot(degrees,rsme_Train)
 axs[1].plot(degrees,rsme_Test)
 axs[1].legend(["Training Data","Test Data"],bbox_to_anchor=(1.35,0.75))
